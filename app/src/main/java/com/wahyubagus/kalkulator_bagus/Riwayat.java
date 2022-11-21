4659c50464181bf326abbc74bf13c769d2a78563
wahyu bagus <bagus8545@gmail.com>
2022-11-21 21:49:52 +0700

    commit tugas kalkulator fix?

---
+++ app/src/main/java/com/wahyubagus/kalkulator_bagus/Riwayat.java
@@ -0,0 +1,31 @@
+package com.wahyubagus.kalkulator_bagus;
+
+import androidx.annotation.NonNull;
+
+public class Riwayat {
+    private String riwayat;
+    private String id;
+
+    public String getRiwayat(){
+        return riwayat;
+    }
+
+    public void setRiwayat(String riwayat){
+        this.riwayat= riwayat;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public Riwayat(String id, String riwayat){
+        this.id = id;
+        this.riwayat = riwayat;
+    }
+
+    @NonNull
+    @Override
+    public String toString(){
+        return riwayat;
+    }
+}
